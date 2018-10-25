1405c0314d3ead63c0a3a01278aaa6ca21c903be
Matthew Phraxayavong <mphraxa@gmail.com>
2018-10-25 10:36:11 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
-
-// app.js
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// app.js
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
