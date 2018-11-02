f05f015a64ec3b46b5208848e81d72edec57152a
Matthew Phraxayavong <mphraxa@gmail.com>
2018-11-01 18:46:18 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost/rotten-potatoes');
+
 
 const Review = mongoose.model('Review', {
   title: String,
+  description: String,
   movieTitle: String
 });
 
+
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // OUR MOCK ARRAY OF PROJECTS
 var reviews = [
@@ -28,6 +33,24 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
+})
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  console.log(req.body);
+  // res.render('reviews-new', {});
+})
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(3000, () => {
