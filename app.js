5404475e8b359abfdeb92797cdb3834e3c0a5935
Matthew Phraxayavong <mphraxa@gmail.com>
2018-11-04 15:03:13 -0800

    Users can destroy reviews

--- app.js
+++ app.js
@@ -63,6 +63,15 @@ app.put('/reviews/:id', (req, res) => {
       console.log(err.message)
     })
 })
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 // CREATE
 // app.post('/reviews', (req, res) => {
