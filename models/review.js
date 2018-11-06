330d9ca38a5e00911e7b7234f09ae56ddc7e4ccc
Matthew Phraxayavong <mphraxa@gmail.com>
2018-11-05 19:03:17 -0800

    Test added

---
+++ models/review.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+
+
+const Review = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String
+});
+
+module.exports = Review;
