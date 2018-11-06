7c2cef4d0e0a36eb23593f20f50e1d8bcdcd4fec
Matthew Phraxayavong <mphraxa@gmail.com>
2018-11-06 13:27:27 -0800

    updates

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -19,37 +19,39 @@ describe('Reviews', ()  => {
          console.log(reviews)
          reviews.remove();
      });
-     });
+ });
 
-    it('should index ALL reviews on / GET', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.should.be.html;
-                done();
-            });
+ it('should index ALL reviews on / GET', (done) => {
+     chai.request(server)
+         .get('/')
+         .end((err, res) => {
+             res.should.have.status(200);
+             res.should.be.html;
+             done();
     });
-    it('should display new form on /reviews/new GET', (done) => {
-    chai.request(server)
-      .get(`/reviews/new`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.html
-          done();
-        });
-  });
-    // it('should show a SINGLE review on /reviews/<id> GET'), (done) => {
-    //     var review = new Review(sampleReview);
-    //     review.save((err, data) => {
-    //         chai.request(server)
-    //             .get(`/reviews/${data._id}`)
-    //             .end((err, res) => {
-    //                 res.should.have.status(200)
-    //                 res.should.be.html
-    //                 done();
-    //             });
-    //     });
+ });
+ it('should create a SINGLE review on /reviews POST', (done) => {
+     chai.request(server)
+         .post('/reviews')
+         .send(sampleReview)
+         .end((err, res) => {
+             res.should.have.status(200);
+             res.should.be.html
+             done();
+     });
+ });
+  it('should show a SINGLE review on /reviews/<id> GET'), (done) => {
+      var review = new Review(sampleReview);
+      review.save((err, data) => {
+          chai.request(server)
+              .get(`/reviews/${data._id}`)
+              .end((err, res) => {
+                  res.should.have.status(200)
+                  res.should.be.html
+                  done();
+              });
+          });
+
     it('should edit a SINGLE review on /reviews/<id>/ edit GET', (done) => {
         var review = new Review(sampleReview);
         review.save((err, data) => {
@@ -59,16 +61,7 @@ describe('Reviews', ()  => {
                 res.should.have.status(200)
                 res.should.be.html
                 done();
-        });
-    });
-    it('should create a SINGLE review on /reviews POST', (done) => {
-        chai.request(server)
-            .post('/reviews')
-            .send(sampleReview)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.should.be.html
-                done();
+            });
         });
     });
     it('should update a SINGLE review on /reviews/<id> PUT', (done) => {
@@ -83,6 +76,18 @@ describe('Reviews', ()  => {
                 done();
         });
     });
-});
-});
+    it('should delete a SINGLE review on /reviews/<id> DELETE', (done) => {
+        var review = new Review(sampleReview);
+        review.save((err, data)  => {
+         chai.request(server)
+          .delete(`/reviews/${data._id}?_method=DELETE`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.html
+            done();
+          });
+        });
+      });
+    });
+};
 });
